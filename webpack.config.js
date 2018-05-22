17cab2cdea113d3f2eaf672def029348bc5a79dd
Lisa Ross <lr@footprintleadership.com>
2018-05-22 12:29:39 -0400

    add page title to html page

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,9 @@ module.exports = {
         filename: 'index_bundle.js'
     },
     plugins: [
-        new HtmlWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            title: 'Quick Favicons'
+        }),
         new FaviconsWebpackPlugin({
             logo: './src/favicon.png',
             emitStats: true,
