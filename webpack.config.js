614091532072b98b8bbdbbd5d34b958b0818bf06
Lisa Ross <lr@footprintleadership.com>
2018-05-22 12:15:00 -0400

    fix path to src/favicon

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
     plugins: [
         new HtmlWebpackPlugin(),
         new FaviconsWebpackPlugin({
-            logo: 'src/favicon.png',
+            logo: './src/favicon.png',
             emitStats: true,
             inject: true,
             prefix: 'icons-[hash]/',
