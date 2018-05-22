bc37fce1ffe15eb5ed7aa00ace24005beceef641
Lisa Ross <lr@footprintleadership.com>
2018-05-22 12:11:55 -0400

    add options to favicon plugin

--- webpack.config.js
+++ webpack.config.js
@@ -3,12 +3,31 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
     entry: 'index.js',
+    mode: 'development',
     output: {
         path: __dirname + '/dist',
         filename: 'index_bundle.js'
     },
     plugins: [
         new HtmlWebpackPlugin(),
-        new FaviconsWebpackPlugin('src/favicon.png')
+        new FaviconsWebpackPlugin({
+            logo: 'src/favicon.png',
+            emitStats: true,
+            inject: true,
+            prefix: 'icons-[hash]/',
+            statsFilename: 'iconstats-[hash].json',
+            icons: {
+                android: true,
+                appleIcon: true,
+                appleStartup: true,
+                coast: false,
+                favicons: true,
+                firefox: true,
+                opengraph: false,
+                twitter: false,
+                yandex: false,
+                windows: false
+            }
+        })
     ]
 }
